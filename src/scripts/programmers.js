642808e78c2ee26473ab881864af5d455d1071ae
3vli <om1ak2345@gmail.com>
2025-01-06 20:11:30 +0200

    Header And Hero components

---
+++ src/scripts/programmers.js
@@ -0,0 +1,17 @@
+const btns = document.querySelectorAll(".select-btns");
+const cardContainers = document.querySelectorAll(".cards");
+console.log(btns);
+btns.forEach((e) => {
+  e.addEventListener("click", () => {
+    if (e.classList.contains("active")) return;
+    btns.forEach((e) => e.classList.remove("active"));
+     e.classList.add("active");
+     
+    const cardContainersArr = [...cardContainers]
+    const itemAttr = e.getAttribute("data-name");
+    if (itemAttr === "all") return cardContainersArr.forEach(card => card.classList.remove("invisible"));
+    else cardContainersArr.forEach(card => card.classList.add("invisible"));
+    const card = cardContainersArr.find(card => card.getAttribute("data-name") === itemAttr);
+    card.classList.remove("invisible")
+  });
+});
