d76b206f1b43a9e920c91c933096679482bf6878
3vli <om1ak2345@gmail.com>
2025-01-10 11:58:31 +0200

    framer motion

---
+++ src/scripts/designers.js
@@ -0,0 +1,42 @@
+import {gsap} from "gsap";
+console.log(gsap);
+// Example Animation
+gsap.to(".card", {
+  duration: 1,
+  y: -50,
+  opacity: 1,
+  stagger: 0.2,
+});
+gsap.from(".design-hero", {
+   x: -500,
+   opacity: 0,
+   stagger: 0.2,
+})
+gsap.to(".design-hero",{
+   duration: 2,
+   x: 0,
+   opacity: 1,
+   stagger: 0.2,
+})
+const btns = document.querySelectorAll(".select-btns");
+const cardContainers = document.querySelectorAll(".cards");
+console.log(btns);
+btns.forEach((e) => {
+  e.addEventListener("click", () => {
+    if (e.classList.contains("active")) return;
+    btns.forEach((e) => e.classList.remove("active"));
+     e.classList.add("active");
+    const cardContainersArr = [...cardContainers]
+    const itemAttr = e.getAttribute("data-name");
+    if (itemAttr === "all") return cardContainersArr.forEach(card => card.classList.remove("invisible"));
+    else cardContainersArr.forEach(card => card.classList.add("invisible"));
+    const card = cardContainersArr.find(card => card.getAttribute("data-name") === itemAttr);
+    card.classList.remove("invisible")
+  });
+});
+const menu = document.querySelector('.nav-container');
+
+function toggle(){
+     menu.classList.toggle('show');
+
+}
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,106 @@
+
+import { gsap } from "gsap";
+
+gsap.from(".hero .info h1", {
+     x: -500,
+     opacity: 0,
+     stagger: 0.2,
+})
+gsap.to(".hero .info h1", {
+     duration: 1,
+     x: 0,
+     opacity: 1,
+     stagger: 0.2,
+})
+
+gsap.from(".hero .info p", {
+     x: -500,
+     opacity: 0,
+     stagger: 0.2,
+})
+gsap.to(".hero .info p", {
+     duration: 3,
+     x: 0,
+     opacity: 1,
+     stagger: 0.2,
+})
+gsap.from(".hero .btn-container a", {
+     opacity: 0,
+     stagger: 0.2,
+})
+gsap.to(".hero .btn-container a", {
+     duration: 3,
+     x: 0,
+     opacity: 1,
+     stagger: 0.2,
+})
+gsap.from(".hero .logo img", {
+     opacity: 0,
+     stagger: 0.2,
+})
+gsap.to(".hero .logo img", {
+     duration: 6,
+     x: 0,
+     opacity: 1,
+     stagger: 0.2,
+})
+gsap.fromTo("#card1",
+     {
+       y: 5,            // Starting position (slightly down)
+     },
+     {
+       y: -5,           // Move 10px up
+       opacity: 1,       // Fade in to full opacity
+       duration: 3,      // Duration of one cycle
+       stagger: {
+         amount: 20,       // Total time for the staggered animation
+         from: "start",   // Stagger starting from the first element
+         axis: "y",       // Stagger along the Y-axis
+       },
+       repeat: -1,       // Infinite repeat
+       yoyo: true,       // Reverse the animation for smooth up and down
+       ease: "power4.inOut", // Smooth easing
+     }
+   );
+   
+   gsap.fromTo("#card2",
+     {
+       y: 5,            // Starting position (slightly down)
+     },
+     {
+       y: -5,           // Move 10px up
+       opacity: 1,       // Fade in to full opacity
+       duration: 2,      // Duration of one cycle
+       stagger: {
+         amount: 20,       // Total time for the staggered animation
+         from: "start",   // Stagger starting from the first element
+         axis: "y",       // Stagger along the Y-axis
+       },
+       repeat: -1,       // Infinite repeat
+       yoyo: true,       // Reverse the animation for smooth up and down
+       ease: "power4.inOut", // Smooth easing
+     }
+   );
+   gsap.fromTo("#card3",
+     {
+       y: 5,            // Starting position (slightly down)
+     },
+     {
+       y: -5,           // Move 10px up
+       opacity: 1,       // Fade in to full opacity
+       duration: 5,      // Duration of one cycle
+       stagger: {
+         amount: 20,       // Total time for the staggered animation
+         from: "start",   // Stagger starting from the first element
+         axis: "y",       // Stagger along the Y-axis
+       },
+       repeat: -1,       // Infinite repeat
+       yoyo: true,       // Reverse the animation for smooth up and down
+       ease: "power4.inOut", // Smooth easing
+     }
+   );
+
+
 const menu = document.querySelector('.nav-container');
 
 function toggle(){
